5ab8d83d565b57cf3be8091208e1ba35672993b7
Richard Taylor <richard@splitsymmetry.com>
2026-09-23 14:13:01 +0930

    Added self-generating BakeID to TerrainNode.

--- Runtime/Nodes/TerrainNode.cs
+++ Runtime/Nodes/TerrainNode.cs
@@ -1,10 +1,46 @@
-﻿using NodeGraph;
+﻿using System;
+using NodeGraph;
 using UnityEngine.Rendering;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace TerrainGraph
 {
     public abstract class TerrainNode : BaseNode
     {
+        private static HashSet<string> activeBakeIDs = new HashSet<string>();
+        
+        [SerializeField] private string bakeId = String.Empty;
+        [NonSerialized] private bool bakeIDInitialized = false;
+
+        public string BakeID
+        {
+            get
+            {
+                if (!bakeIDInitialized)
+                {
+                    while (string.IsNullOrEmpty(bakeId) || !activeBakeIDs.Add(bakeId))
+                        bakeId = GenerateBakeID();
+
+                    bakeIDInitialized = true;
+                }
+
+                return bakeId;
+            }
+        }
+
+        public string GenerateBakeID()
+        {
+            string bakeId = Guid.NewGuid().ToString();
+            return bakeId.Substring(bakeId.LastIndexOf('-') + 1);
+        }
+
+        public override void Cleanup()
+        {
+            base.Cleanup();
+            activeBakeIDs.Remove(bakeId);
+        }
+        
         protected float GetConnectionMin(string fieldName)
         {
             var node = GetConnectedNode(fieldName) as TerrainInputNode;
